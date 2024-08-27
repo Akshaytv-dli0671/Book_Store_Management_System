3a3bdbe7dc947e0d34e5918b719bbb9c30c94e74
sidra_syed <Sidra.S@godigit.com>
2024-08-27 11:53:35 +0530

    This is Admin Module

--- src/main/java/com/BSMS/Book_Store_ManagementSystem/model/Admin.java
+++ src/main/java/com/BSMS/Book_Store_ManagementSystem/model/Admin.java
@@ -1,4 +1,34 @@
 package com.BSMS.Book_Store_ManagementSystem.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "admin_table")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "username is mandatory")
+    private String adminUsername;
+
+@NotNull(message = "Email is mandatory")
+    private String adminEmail;
+
+@NotNull(message = "Password is mandatory")
+    private String adminPassword;
+
+@NotNull(message = "created_at is mandatory")
+    private Timestamp created_at;
+
 }
