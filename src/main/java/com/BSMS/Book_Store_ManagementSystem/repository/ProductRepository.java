8b078d94bb23bc9fa6761dd3275d3d1a930c01aa
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-08-27 17:06:03 +0530

    product repository

--- src/main/java/com/BSMS/Book_Store_ManagementSystem/repository/ProductRepository.java
+++ src/main/java/com/BSMS/Book_Store_ManagementSystem/repository/ProductRepository.java
@@ -4,8 +4,6 @@ import com.BSMS.Book_Store_ManagementSystem.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Products,Long> {
-//    Optional<Products> findByName(String productName);
 }
