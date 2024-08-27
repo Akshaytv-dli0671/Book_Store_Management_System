654d60588425a89235acf755e60abac30d8a7ca5
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-08-27 12:14:48 +0530

    This is Products Module

---
+++ src/main/java/com/BSMS/Book_Store_ManagementSystem/model/Products.java
@@ -0,0 +1,37 @@
+package com.BSMS.Book_Store_ManagementSystem.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name="products_table")
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String productName;
+
+    private String productDescription;
+
+    private double productPrice;
+
+    @NotNull
+    private String productAuthor;
+
+    @NotNull
+    private int stock;
+
+    private String productCategory;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+}
---
+++ src/main/java/com/BSMS/Book_Store_ManagementSystem/repository/ProductRepository.java
@@ -0,0 +1,2 @@
+package com.BSMS.Book_Store_ManagementSystem.repository;public interface ProductRepository {
+}
